c35de787aaf8548f085fac7e4b82b20eab301656
Alex Crichton <alex@alexcrichton.com>
2025-02-20 18:28:58 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,1 @@
-fn main() {
-    for f in cranelift_assembler_x64::generated_files() {
-        dbg!(&f);
-        if f.extension().and_then(|s| s.to_str()) == Some("isle") {
-            assert!(f.exists());
-        }
-    }
-}
+fn main() {}
